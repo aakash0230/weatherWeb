cf1920671eae89f185f96c71d0d50850c4d17c47
Aakash <Pandeyaakash123.ap@gmail.com>
2021-10-15 17:20:56 +0530

    just testing the commit

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.get("/weather", (req, res) => {
 
 app.get("*", (req, res) => {
     res.render('404error', {
-        errorMsg : 'OOps! page not found'
+        errorMsg : 'OOps! page not found,......'
     });
 });
 
